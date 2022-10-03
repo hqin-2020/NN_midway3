35238bb0fe98cc980dc05e509d23fac8084104ac
hqin-2020 <uctphqi@ucl.ac.uk>
2022-10-02 22:59:12 -0500

    1

--- src/moment_NN_midway3.sh
+++ src/moment_NN_midway3.sh
@@ -10,7 +10,7 @@ sigma_K_norm=0.04
 sigma_Z_norm=0.0141
 sigma_V_norm=0.0
 sigma_Vtilde_norm=0.1
-trial=1
+trial=2
 weight1=0.0
 boundary1=2
 weight2=0.0
@@ -33,19 +33,19 @@ else
     mkdir -p ./bash/$domain_folder
 fi
 
-for chiUnderline in 1.0
+for chiUnderline in 0.5 1.0 0.2
 do 
     for a_e in 0.15
     do
-        for a_h in 0.13 0.1 0.05
+        for a_h in 0.1 0.05
         do
-            for gamma_e in 1.0
+            for gamma_e in 0.5
             do
-                for gamma_h in 1.0
+                for gamma_h in 8.0
                 do
-                    for psi_e in 1.0
+                    for psi_e in 0.5 1.5
                     do
-                        for psi_h in 1.0
+                        for psi_h in 0.5 1.5
                         do
                             model_folder=chiUnderline_${chiUnderline}_a_e_${a_e}_a_h_${a_h}_gamma_e_${gamma_e}_gamma_h_${gamma_h}_psi_e_${psi_e}_psi_h_${psi_h}
                             mkdir -p ./job-outs/$domain_folder/$model_folder
@@ -82,15 +82,14 @@ do
 #SBATCH --output=./job-outs/$domain_folder/$model_folder/$layer_folder/moment.out
 #SBATCH --error=./job-outs/$domain_folder/$model_folder/$layer_folder/moment.err
 #SBATCH --time=0-24:00:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --partition=caslake
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=14
-#SBATCH --mem=64G
+#SBATCH --mem=56G
 
 module load python/anaconda-2022.05
-module load cuda/11.2
 
-python3 standard_moments.py   --chiUnderline ${chiUnderline} --a_e ${a_e} --a_h ${a_h} --gamma_e ${gamma_e} --gamma_h ${gamma_h} --psi_e ${psi_e} --psi_h ${psi_h} --nWealth ${nWealth} --nZ ${nZ} --nV ${nV} --nVtilde ${nVtilde} --V_bar ${V_bar} --Vtilde_bar ${Vtilde_bar} --sigma_V_norm ${sigma_V_norm} --sigma_Vtilde_norm ${sigma_Vtilde_norm} --XiE_layers ${XiE_layers} --XiH_layers ${XiH_layers} --kappa_layers ${kappa_layers} --weight1 ${weight1} --boundary1 ${boundary1} --weight2 ${weight2} --boundary2 ${boundary2} --points_size ${points_size} --iter_num ${iter_num} --trial ${trial} --chi_position_tolerance ${chi_position_tolerance} --chi_value_tolerance ${chi_value_tolerance} --chi_max_iterations ${chi_max_iterations} --W_fix ${W_fix} --Z_fix ${Z_fix} --V_fix ${V_fix} --Vtilde_fix ${Vtilde_fix} 
+python3 standard_moments.py   --chiUnderline ${chiUnderline} --a_e ${a_e} --a_h ${a_h} --gamma_e ${gamma_e} --gamma_h ${gamma_h} --psi_e ${psi_e} --psi_h ${psi_h} --nWealth ${nWealth} --nZ ${nZ} --nV ${nV} --nVtilde ${nVtilde} --V_bar ${V_bar} --Vtilde_bar ${Vtilde_bar} --sigma_V_norm ${sigma_V_norm} --sigma_Vtilde_norm ${sigma_Vtilde_norm} --XiE_layers ${XiE_layers} --XiH_layers ${XiH_layers} --kappa_layers ${kappa_layers} --weight1 ${weight1} --boundary1 ${boundary1} --weight2 ${weight2} --boundary2 ${boundary2} --points_size ${points_size} --iter_num ${iter_num} --trial ${trial} --chi_position_tolerance ${chi_position_tolerance} --chi_value_tolerance ${chi_value_tolerance} --chi_max_iterations ${chi_max_iterations} --W_fix ${W_fix} --Z_fix ${Z_fix} --V_fix ${V_fix} --Vtilde_fix ${Vtilde_fix}
 
 EOF
                                                                 sbatch ./bash/$domain_folder/$model_folder/$layer_folder/moment.sh
